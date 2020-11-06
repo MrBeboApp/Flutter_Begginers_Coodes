e8d15207ebdcd1bf6ce48d46c14dc73bcedf4873
Bahaa Ehab El Dien Abdelmawla <mbcompany888@gmail.com>
2020-11-06 19:20:34 +0200

    add the food section

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -40,14 +41,28 @@ class HomePage extends StatelessWidget {
                       ),
                     ),
                   )),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 122,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: titles.length,
+                      itemBuilder: (context,index){
+
+                    return _sectionCard(icons[index],titles[index]);
+                  }),
+                ),
+              ),
+              Text("Popular Food",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
               Container(
                 height: 200,
+
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
-                  itemCount: titles.length,
+                  itemCount: 5,
                     itemBuilder: (context,index){
-
-                  return _sectionCard(icons[index],titles[index]);
+                  return _foodCard();
                 }),
               )
             ],
@@ -75,6 +90,70 @@ class HomePage extends StatelessWidget {
           SizedBox(height: 10,),
           Text(title)
         ],
+      ),
+    );
+  }
+
+  Widget _foodCard() {
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            color: Colors.white,
+            width: 200,
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        image: DecorationImage(
+
+                          image: NetworkImage("https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"),fit: BoxFit.cover,
+                        )
+                      ),
+                      ),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Pasta With Ham",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
+                        Row(children: [
+                          Text("4.2",style: TextStyle(color: Colors.grey)),
+                          Row(children: [
+                            Icon(Icons.star,color: Colors.indigo,size: 15,),
+                            Icon(Icons.star,color: Colors.indigo,size: 15),
+                            Icon(Icons.star,color: Colors.indigo,size: 15),
+                            Icon(Icons.star,color: Colors.indigo,size: 15),
+                            Icon(Icons.star,color: Colors.grey,size: 15),
+                          ],),
+                          Text("(12+)",style: TextStyle(color: Colors.grey)),
+
+                        ],)
+                      ],
+                    ),
+                    Text("\$25",style: TextStyle(color: Colors.indigo,fontWeight: FontWeight.bold)),
+
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+
       ),
     );
   }
